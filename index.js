bc3eff062a1497d8b9889b65686fb05919f202c2
zvdas <rodriguezzude@gmail.com>
2023-01-07 23:57:42 +0530

    friends crud successful

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const routes = require('./router/friends.js')
 
 let users = []
 
-const doesExist = (username)=>{
-  let userswithsamename = users.filter((user)=>{
+const doesExist = (username) => {
+  let userswithsamename = users.filter((user) => {
     return user.username === username
   });
   if(userswithsamename.length > 0){
@@ -16,8 +16,8 @@ const doesExist = (username)=>{
   }
 }
 
-const authenticatedUser = (username,password)=>{
-  let validusers = users.filter((user)=>{
+const authenticatedUser = (username,password) => {
+  let validusers = users.filter((user) => {
     return (user.username === username && user.password === password)
   });
   if(validusers.length > 0){
@@ -34,20 +34,20 @@ app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
 app.use(express.json());
 
 app.use("/friends", function auth(req,res,next){
-   if(req.session.authorization) {
-       token = req.session.authorization['accessToken'];
-       jwt.verify(token, "access",(err,user)=>{
-           if(!err){
-               req.user = user;
-               next();
-           }
-           else{
-               return res.status(403).json({message: "User not authenticated"})
-           }
-        });
-    } else {
-        return res.status(403).json({message: "User not logged in"})
-    }
+  if(req.session.authorization) {
+    token = req.session.authorization['accessToken'];
+    jwt.verify(token, "access",(err,user) => {
+      if(!err){
+        req.user = user;
+        next();
+      }
+      else{
+        return res.status(403).json({message: "User not authenticated"})
+      }
+    });
+  } else {
+    return res.status(403).json({message: "User not logged in"})
+  }
 });
 
 app.post("/login", (req,res) => {
@@ -79,7 +79,7 @@ app.post("/register", (req,res) => {
   if (username && password) {
     if (!doesExist(username)) { 
       users.push({"username":username,"password":password});
-      return res.status(200).json({message: "User successfully registred. Now you can login"});
+      return res.status(200).json({message: "User successfully registered. Now you can login"});
     } else {
       return res.status(404).json({message: "User already exists!"});    
     }
@@ -87,9 +87,8 @@ app.post("/register", (req,res) => {
   return res.status(404).json({message: "Unable to register user."});
 });
 
-
-const PORT =5000;
+const PORT = 5000;
 
 app.use("/friends", routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- router/friends.js
+++ router/friends.js
@@ -3,45 +3,70 @@ const express = require('express');
 const router = express.Router();
 
 var friends = {
-    "johnsmith@gamil.com": {"firstName": "John","lastName": "Doe","DOB":"22-12-1990"},
-    "annasmith@gamil.com":{"firstName": "Anna","lastName": "smith","DOB":"02-07-1983"},
-    "peterjones@gamil.com":{"firstName": "Peter","lastName": "Jones","DOB":"21-03-1989"}
+  "johnsmith@gamil.com": {"firstName": "John","lastName": "Doe","DOB":"22-12-1990"},
+  "annasmith@gamil.com":{"firstName": "Anna","lastName": "smith","DOB":"02-07-1983"},
+  "peterjones@gamil.com":{"firstName": "Peter","lastName": "Jones","DOB":"21-03-1989"}
 };
 
-
 // GET request: Retrieve all friends
-router.get("/",(req,res)=>{
-
+router.get("/", (req,res)=>{
   // Update the code here
-
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(JSON.stringify(friends,null,4)); //This line is to be replaced with actual return value
 });
 
 // GET by specific ID request: Retrieve a single friend with email ID
-router.get("/:email",(req,res)=>{
+router.get("/:email", (req,res)=>{
   // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  res.send(friends[email]); //This line is to be replaced with actual return value
 });
-
 
 // POST request: Add a new friend
 router.post("/",(req,res)=>{
   // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  if(req.body.email) {
+    friends[req.body.email] = {
+      "firstname": req.body.firstName,
+      "lastname": req.body.lastName,
+      "DOB": req.body.DOB,
+    }
+  }
+  res.send("The user" + (' ')+ (req.body.firstName) + " Has been added!"); //This line is to be replaced with actual return value
 });
-
 
 // PUT request: Update the details of a friend with email id
 router.put("/:email", (req, res) => {
   // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  let friend = friends[email];
+  if(friend) {
+    //Check is friend exists
+    let { firstName, lastName, DOB } = req.body;
+    //if DOB the DOB has been changed, update the DOB
+    if(DOB) {
+      friend["DOB"] = DOB
+    }
+    if(firstName) {
+      friend["firstName"] = firstName
+    }
+    if(lastName) {
+      friend["lastName"] = lastName
+    }
+    friends[email] = friend;
+    res.send(`Friend with the email ${email} updated`); //This line is to be replaced with actual return value
+  } else {
+    res.send("Unable to find friend!");
+  }
 });
-
 
 // DELETE request: Delete a friend by email id
 router.delete("/:email", (req, res) => {
   // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  if(email) {
+    delete friends[email]
+  }
+  res.send(`Friend with the email ${email} deleted.`); //This line is to be replaced with actual return value
 });
 
 module.exports=router;
